1a140d31e33e67fd15e48fb99f33bee8b74c0f49
hyago1 <hyagophilip@gmail.com>
2023-09-26 23:56:37 -0300

    Update

---
+++ script.js
@@ -0,0 +1,34 @@
+var msg = {
+    "msg":[]
+
+}
+var boxmsg = document.getElementById('box_Msg')
+var list = document.getElementById('list_ul')
+
+
+
+
+
+
+
+
+
+
+
+function teste() {
+   
+list.innerHTML = ""
+msg.msg.push(boxmsg.value)
+
+msg.msg.map((value) =>{
+ 
+    list.innerHTML +=   `<li>
+<div class="ball_msg">
+    <span class="nickname_ball_msg">Ze</span>
+    <span class="msg">${value}</span>
+</div>
+</li>`
+    })
+
+}
+
