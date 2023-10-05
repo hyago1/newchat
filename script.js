8527f67e2aa35f92d014fc233a84423ecef2bc99
hyago1 <hyagophilip@gmail.com>
2023-10-05 11:59:56 -0300

    update

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ const updateList = () => {
       let datamsg = value.datamsg
       console.log(value);
 if (value.datamsg.startsWith("http://") || value.datamsg.startsWith("https://")) {
-datamsg = `<a src='${value.datamsg}'></a>`
+datamsg = `<a src='${value.datamsg}'>${value.datamsg}</a>`
 }
 
 
